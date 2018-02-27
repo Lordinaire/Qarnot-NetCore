6da9e635cb08dda2dc224fb7ee269364e849ef04
Maxime Frappat <m.frappat@sav-group.com>
2018-02-27 09:34:31 +0100

    Add .NET Core app sources

---
+++ netcoreapp/Rendering/Program.cs
@@ -0,0 +1,18 @@
+﻿using SixLabors.ImageSharp;
+
+namespace Rendering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Image<Rgba32> image = Image.Load(args[0]))
+            {
+                image.Mutate(x => x
+                    .OilPaint()
+                    .Grayscale());
+                image.Save($"new_{args[0]}");
+            }
+        }
+    }
+}
